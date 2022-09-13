fe6902ef13c56da43a150f0fdf435af2e415f242
kevin <yanchenkai@gmail.com>
2022-09-12 22:52:16 -0400

    finished 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s\']|_/g, "");
+    str = str.replace(/\s+/g, "");
+    str = str.toLowerCase();
+    let cnt = str.length-1;
+    for(let i = 0; i < str.length/2; i++){
+        if(!(str.charAt(i) === str.charAt(i+cnt))){
+            return false;
+        }
+        cnt -= 2;
+    }
+    return true;
 };
 
 // Do not edit below this line
