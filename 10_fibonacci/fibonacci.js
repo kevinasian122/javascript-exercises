69cbfd6500a5c5123d837917a6855c3cad753bd4
kevin <yanchenkai@gmail.com>
2022-09-13 14:18:24 -0400

    did 09, 10, 11 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if(num<=0) return "OOPS";
+    let temp = 1;
+    let back = 1;
+    let front = 1;
+    for(let i = 2; i < num; i++){
+        temp = front;
+        front = front + back;
+        back = temp;
+        
+    }
+    return front;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let age = 0;
+        if(!oldest.yearOfDeath) age = Date-oldest.yearOfBirth;
+        else age = oldest.yearOfDeath-oldest.yearOfBirth;
+        if(!person.yearOfDeath){
+            if(Date-person.yearOfBirth > age){
+                return person;
+            }
+        }
+        else if(person.yearOfDeath-person.yearOfBirth > age){
+            return person;
+        }
+    });
 };
 
 // Do not edit below this line
